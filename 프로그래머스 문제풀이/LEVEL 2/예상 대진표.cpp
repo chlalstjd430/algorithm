5f0024374fc7b1754182fc25e563de36dffa19b8
MinSeong <50758600+chlalstjd430@users.noreply.github.com>
2020-03-17 21:45:26 +0900

    Create 예상 대진표.cpp

---
+++ 프로그래머스 문제풀이/LEVEL 2/예상 대진표.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+    a--;
+    b--;
+    while(a != b){
+        answer++;
+        a /= 2;
+        b /= 2;
+    }
+
+    return answer;
+}
