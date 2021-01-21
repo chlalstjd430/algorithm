23b00f43cfead706108db0812f34423bcd986d11
chlalstjd430 <cms3136@gmail.com>
2021-01-22 01:15:40 +0900

    동적 계획법 문제 업데이트

---
+++ 종만북 문제풀이/08. 동적 계획법/02. 문제/03_원주율_외우기.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+const int INF = 987654321;
+string str;
+int cache[10002];
+
+int classify(int a, int b) {
+  // 숫자 조각을 가져온다
+  string tmp = str.substr(a, b - a + 1);
+
+  // 난이도 1 : 첫 글자만으로 이루어진 문자열과 같으면 난이도는 1
+  if (tmp == string(tmp.size(), tmp[0])) {
+    return 1;
+  }
+
+  // 등차수열인지 확인
+  bool flag = true;
+  for(int i = 0; i < tmp.size() - 1; i++) {
+    if(tmp[i+1] - tmp[i] != tmp[1] - tmp[0]) {
+      flag = false;
+      break;
+    }
+  }
+
+  // 난이도 2 : 등차수열이고 공차가 1인 경우 -> 
+  if (flag && abs(tmp[1] - tmp[0])) {
+    return 2;
+  }
+
+  // 두 수가 번갈아 등장하는지 확인
+  bool flagB = true;
+  for (int i = 0; i < tmp.size(); i++) {
+    if (tmp[i] != tmp[i%2]) {
+      flagB = false;
+      break;
+    }
+  }
+
+  // 난이도 4 : 두 수가 번갈아가는 경우
+  if (flagB) return 4;
+
+  // 난이도 5 : 등차 수열
+  if (flag) return 5;
+
+  // 난이도 10 : 그 외
+  return 10;
+}
+
+int memorize(int pos) {
+  // 기저사례
+  if (pos == str.size()) return 0;
+  
+  int ret = cache[pos];
+  if (ret != -1) return ret;
+  ret = 1;
+
+  for (int i = 3; i <= 5; i++) {
+    ret = min(ret, memorize(pos + i) + classify(pos, pos + i - 1));
+  }
+
+  return cache[pos] = ret;
+}
+
+int main() {
+
+}
+
+/*
+  풀이방법
+
+
+*/
